a4ef5bda14aa8baff7e4d85d475ad82c461cd311
awdr83 <wjsth26@naver.com>
2020-08-07 03:29:36 +0900

    branch test

--- src/containers/BusInfoPage.js
+++ src/containers/BusInfoPage.js
@@ -14,6 +14,7 @@ class BusInfoPage extends Component {
         return (
             <Fragment>
                 <div>hello world</div>
+                <div>commit test</div>
             </Fragment>
         )
     }
